d3d018c732ccd32fa51ed1d69925ba9b68d7cf2a
JanLascala <lascala.jan@gmail.com>
2025-04-17 09:34:54 +0200

    fix: wrap image in Link component for better navigation in Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Cards({ id, title, image }) {
     return (
+
         <div className="card">
-            <img src={`http://localhost:3000/${image}`} alt={title} className="card-img-top" />
+            <Link to={`/${id}`}>
+                <img src={`http://localhost:3000/${image}`} alt={title} className="card-img-top" />
+            </Link>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <Link to={`/${id}`} className="btn btn-primary">
