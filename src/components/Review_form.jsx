463a98813f971abe43293604b7ec49cc7e944758
JanLascala <lascala.jan@gmail.com>
2025-04-22 17:01:05 +0200

    fix: correct form handling and state management in Review_form component

--- src/components/Review_form.jsx
+++ src/components/Review_form.jsx
@@ -1,14 +1,36 @@
+import { useState, useEffect } from "react"
 export default function Review_form() {
+
+    const initialFormdata = {
+        name: "",
+        vote: 1,
+        text: "",
+    };
+    const [formData, setFormData] = useState(initialFormdata);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
 
         <form>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                <input type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Enter your name"
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
             </div>
             <div className="mb-3">
                 <label htmlFor="vote" className="form-label">Vote</label>
-                <select className="form-select" id="vote">
+                <select className="form-select"
+                    id="vote"
+                    value={formData.vote}
+                    onChange={(e) => setFormData({ ...formData, vote: e.target.value })}>
+
                     <option value="">Select a rating</option>
                     <option value="1">1 - Poor</option>
                     <option value="2">2 - Fair</option>
@@ -19,9 +41,17 @@ export default function Review_form() {
             </div>
             <div className="mb-3">
                 <label htmlFor="text" className="form-label">Review</label>
-                <textarea className="form-control" id="text" rows="4" placeholder="Write your review here"></textarea>
+                <textarea className="form-control"
+                    id="text"
+                    rows="4"
+                    placeholder="Write your review here"
+                    value={formData.text}
+                    onChange={(e) => setFormData({ formData, text: e.target.value })}></textarea>
             </div>
-            <button type="submit" className="btn btn-primary">Submit Review</button>
+            <button type="submit"
+                className="btn btn-primary"
+                onSubmit={handleSubmit}>
+                Submit Review</button>
         </form>
 
     )
