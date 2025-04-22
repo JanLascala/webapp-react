197b289461413279b7a5d7890b44b12405daae05
JanLascala <lascala.jan@gmail.com>
2025-04-22 17:16:46 +0200

    feat: add form validation and submission logic in Review_form component

--- src/components/Review_form.jsx
+++ src/components/Review_form.jsx
@@ -10,49 +10,67 @@ export default function Review_form() {
 
     function handleSubmit(e) {
         e.preventDefault();
+
     }
+    //fetch
+    fetch("http://localhost:3000/api/v1/reviews", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Data Successfuly submitted", data);
 
-    return (
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
 
-        <form>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">Name</label>
-                <input type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Enter your name"
-                    value={formData.name}
-                    onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="vote" className="form-label">Vote</label>
-                <select className="form-select"
-                    id="vote"
-                    value={formData.vote}
-                    onChange={(e) => setFormData({ ...formData, vote: e.target.value })}>
 
-                    <option value="">Select a rating</option>
-                    <option value="1">1 - Poor</option>
-                    <option value="2">2 - Fair</option>
-                    <option value="3">3 - Good</option>
-                    <option value="4">4 - Very Good</option>
-                    <option value="5">5 - Excellent</option>
-                </select>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label">Review</label>
-                <textarea className="form-control"
-                    id="text"
-                    rows="4"
-                    placeholder="Write your review here"
-                    value={formData.text}
-                    onChange={(e) => setFormData({ formData, text: e.target.value })}></textarea>
-            </div>
-            <button type="submit"
-                className="btn btn-primary"
-                onSubmit={handleSubmit}>
-                Submit Review</button>
-        </form>
+return (
 
-    )
+    <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter your name"
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+        </div>
+        <div className="mb-3">
+            <label htmlFor="vote" className="form-label">Vote</label>
+            <select className="form-select"
+                id="vote"
+                value={formData.vote}
+                onChange={(e) => setFormData({ ...formData, vote: e.target.value })}>
+
+                <option value="">Select a rating</option>
+                <option value="1">1 - Poor</option>
+                <option value="2">2 - Fair</option>
+                <option value="3">3 - Good</option>
+                <option value="4">4 - Very Good</option>
+                <option value="5">5 - Excellent</option>
+            </select>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="text" className="form-label">Review</label>
+            <textarea className="form-control"
+                id="text"
+                rows="4"
+                placeholder="Write your review here"
+                value={formData.text}
+                onChange={(e) => setFormData({ ...formData, text: e.target.value })}></textarea>
+        </div>
+        <button type="submit"
+            className="btn btn-primary">
+            Submit Review</button>
+    </form>
+
+)
 }
